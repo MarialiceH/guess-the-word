13578848cc485056c6f8d292f4d2b0da0d9ecba3
MarialiceH <137061846+MarialiceH@users.noreply.github.com>
2023-07-18 15:20:01 -0400

    Create a Function to Show the Guessed Letters

--- js/script.js
+++ js/script.js
@@ -62,5 +62,16 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        showGuessedLetters();
 }
 };
+
+const showGuessedLetters = function () {
+    //empty the innerHTML of the unordered list where the player's guessed letters will display
+    guessedLettersElement.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersElement.append(li);
+    }
+}
